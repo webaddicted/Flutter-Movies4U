4604a6345433f9e50a0be3e891ef4f8c67eae6de
Deepak Sharma <deepaksharma040695@gmail.com>
2020-11-27 22:22:48 +0530

    Add feedback, invite

--- lib/view/other/about_us_screen.dart
+++ lib/view/other/about_us_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movies4u/constant/api_constant.dart';
+import 'package:movies4u/constant/color_const.dart';
 import 'package:movies4u/constant/string_const.dart';
 import 'package:movies4u/utils/widgethelper/widget_helper.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -14,8 +15,19 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
 
   @override
   Widget build(BuildContext context) {
+      var homeIcon = IconButton(
+        icon: Icon(
+          Icons.arrow_back_ios,
+          color:ColorConst.BLACK_COLOR,
+        ),
+        onPressed: () => Navigator.pop(context));
     return Scaffold(
-      appBar: getAppBarWithBackBtn(ctx:context,title: 'About Us'),
+      appBar: getAppBarWithBackBtn(
+          ctx: context,
+          title: 'About Us',
+          bgColor: ColorConst.WHITE_BG_COLOR,
+          titleTag: 'About Us',
+          icon: homeIcon),
       body: Builder(builder: (_context) => _createUi(_context)),
     );
   }
